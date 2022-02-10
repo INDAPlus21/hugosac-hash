53e3981f65f88a9dbb7942d6ce56f5b83dd38574
Hugo Sacilotto <hugge.saci@gmail.com>
2022-02-10 12:26:09 +0100

    Refactor hash table to struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,142 @@
 use std::collections::LinkedList;
 
-
 struct Data {
     pub key: String,
     pub value: String
+}
+
+struct HashTable {
+    pub table: Vec<Option<LinkedList<Data>>>,
+    pub num_of_lists: usize,
+    pub num_of_elements: usize,
+    pub capacity: usize
+}
+
+impl HashTable {
+    pub fn new(capacity: usize) -> HashTable {
+        let mut hash_table: Vec<Option<LinkedList<Data>>> = Vec::with_capacity(capacity);
+        for _ in 0..capacity {
+            hash_table.push(None);
+        }
+        
+        HashTable {
+            table: hash_table,
+            num_of_lists: 0,
+            num_of_elements: 0,
+            capacity: capacity
+        }
+    }
+
+    pub fn add(&mut self, element: Data) {
+        let index: usize = hashcode(&element.key) % self.capacity;
+
+        match &mut self.table[index] {
+            Some(list) => {
+                list.push_front(element);
+                self.num_of_elements += 1;
+            }
+            None => { 
+                self.table[index] = Some(LinkedList::from([element]));
+                self.num_of_lists += 1;
+                self.num_of_elements += 1;
+            }
+        }
+    }
+
+    pub fn insert(&mut self, key: &String, value: &String) {
+        // Check if key is already used
+        if self.get(key) != String::from("Key is not present in the database") {
+            println!("Key is already used");
+            return;
+        }
+        
+        if self.num_of_lists == self.capacity - 1 {
+            println!("Expansion is needed");
+            // Expand table
+            self.capacity *= 2;
+            let mut new_table = HashTable::new(self.capacity);
+
+            // Copy all elements 
+            for _list in &self.table {
+                match _list {
+                    Some(list) => {
+                        for element in list.iter() {
+                            new_table.insert(&element.key, &element.value);
+                        }
+                    }
+                    None => {}
+                }
+            }
+
+            *self = new_table;
+            println!("Table has been rebuilt!");
+        }
+
+        let index: usize = hashcode(key) % self.capacity; 
+
+        match &mut self.table[index] {
+            Some(list) => {
+                list.push_front(
+                    Data { 
+                        key: key.to_string(), 
+                        value: value.to_string() 
+                    }
+                );
+                self.num_of_elements += 1;
+            }
+            None => { 
+                self.table[index] = Some(LinkedList::from(
+                    [
+                        Data {
+                            key: key.to_string(),
+                            value: value.to_string()
+                        }
+                    ]
+                ));
+                self.num_of_lists += 1;
+                self.num_of_elements += 1;
+            }
+        }
+    }
+
+    pub fn get(&mut self, key: &String) -> String {
+        let index = hashcode(key) % self.capacity;
+        match &mut self.table[index] {
+            Some(list) => {
+                for element in list.iter() {
+                    if &element.key == key {
+                        return element.value.to_string();
+                    }
+                }
+                return String::from("Key is not present in the database");
+            }
+            None => {
+                return String::from("Key is not present in the database");
+            }
+        }
+    }
+
+    pub fn index(&mut self, key: &String) -> i32 {
+        let index = hashcode(key) % self.capacity;
+        match &mut self.table[index] {
+            Some(list) => {
+                for element in list.iter() {
+                    if &element.key == key {
+                        return index as i32;
+                    }
+                }
+
+                return -1;
+            }
+            None => {
+                return -2;
+            }
+
+            _ => {
+                return -3;
+            }
+        }
+    }
 }
 
 
@@ -22,67 +155,24 @@ fn hashcode(key: &String) -> usize {
 }
 
 
-
-
-fn insert(hash_table: &mut Vec<Option<LinkedList<Data>>>, key: &String, value: &String) {
-    // if hash_table.len() == hash_table.capacity() - 1 {
-    //     // Expand table
-    //     // Move all entries to new hash table
-
-    //     // Copy table
-    //     let mut hash_table_copy: Vec<LinkedList<Data>> = Vec::with_capacity(hash_table.capacity() * 2);
-
-    //     // Add all data to the copy with new hashing indices
-    //     for _i in 0..hash_table.len() {
-    //         if hash_table[_i].len() > 0 {
-    //             for element in hash_table[_i].iter() {
-    //                 let _index = hashcode(&element.key) % hash_table_copy.capacity();
-    //                 let data = element;
-    //                 hash_table_copy[_index].push_front(*data);
-    //             }
-    //         }
-    //     }
-
-    //     hash_table.clear();
-    //     let new_table = hash_table_copy;
-    //     // hash_table = hash_table_copy;
-    // }
-
-    let index: usize = hashcode(key) % hash_table.capacity();
-
-    match &mut hash_table[index] {
-        Some(inner) => {
-            inner.push_front(
-                Data { key: key.to_string(), value: value.to_string() }
-            );           
-        },
-        None => {
-            hash_table[index] = Some(LinkedList::from(
-                [
-                    Data {
-                        key: key.to_string(),
-                        value: value.to_string()
-                    }
-                ]
-            ));
-        }
-    }
-
-}
-
 fn main() {
 
-    // let mut hash_table: Vec<LinkedList<Data>> = Vec::with_capacity(10);
-    let mut hash_table: Vec<Option<LinkedList<Data>>> = Vec::with_capacity(20);
-    for _ in 0..hash_table.capacity() {
-        hash_table.push(None);
-    }
-    // println!("{}", hash_table.capacity());
-    insert(&mut hash_table, &String::from("Hugo"), &String::from("Sacilotto"));
-    insert(&mut hash_table, &String::from("Axel"), &String::from("Andersson"));
-    // println!("{}", hash_table.len());
-
-    println!("{}", hash_table[hashcode(&String::from("Hugo")) % hash_table.capacity()].as_ref().unwrap().iter().next().unwrap().value);
-    println!("{}", hash_table[hashcode(&String::from("Axel")) % hash_table.capacity()].as_ref().unwrap().iter().next().unwrap().value);
-
+    let capacity = 3;
+    let mut hash_table = HashTable::new(capacity);
+    hash_table.insert(&String::from("Hugo"), &String::from("Sacilotto"));
+    println!("{}", hash_table.index(&String::from("Hugo")));
+    hash_table.insert(&String::from("Hug"), &String::from("Saci"));
+    hash_table.insert(&String::from("go"), &String::from("Saci"));
+    hash_table.insert(&String::from("ugo"), &String::from("Saci"));
+    hash_table.insert(&String::from("Hu"), &String::from("Saci"));
+    hash_table.insert(&String::from("Huuugo"), &String::from("Saci"));
+    hash_table.insert(&String::from("Huggo"), &String::from("Saci"));
+    hash_table.insert(&String::from("Huggggo"), &String::from("Saci"));
+    println!("{}", hash_table.index(&String::from("Hugo")));
+    
+    
+    // let value = hash_table.get(&String::from("Hugo"));
+    // println!("{}", value);
+    // println!("{}", hash_table.num_of_elements);
+    // println!("{}", hash_table.num_of_lists);
 }
